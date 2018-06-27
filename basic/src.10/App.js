63d88ac71ea71b8da7b5171bdb2edf7dd0bf8dab
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-27 15:12:05 -0700

    Styling using both stylesheets and in-line styles.

---
+++ basic/src.10/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Chomper", age: 3},
+      {name: "Critter", age: 10},
+      {name: "Toffe", age: 1}
+      ],
+      otherState: false
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState( {
+      persons: [
+        {name: newName, age: 5},
+        {name: "Critter", age: 10},
+        {name: "Toffe", age: 1}
+        ]}
+      );
+  };
+
+  nameChangdHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'Chomper', age: 5},
+        {name: event.target.value, age: 10},
+        {name: "Toffe", age: 1}
+        ]}
+      );    
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>This is a basic react app.</h1>
+        {/* <button onClick={this.switchNameHandler.bind(this, 'Nuzzy')}>Switch name</button>   -- perferred */}
+
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Thor')}>Switch name</button>
+
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Goldy Bear')}
+          changed={this.nameChangdHandler}>Gray and white tabby cat.</Person>
+
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+export default App;
