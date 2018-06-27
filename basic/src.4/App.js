a3ef1198d5d2c524bcfac4b44038153d164c0289
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-27 11:34:57 -0700

    Working with prop (properties).

---
+++ basic/src.4/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is a basic react app.</h1>
+        <Person name="Chomper" age="3"/>
+        <Person name="Critter" age="10">Cat</Person>
+        <Person name="Toffe" age="1"/>
+      </div>
+    );
+  }
+}
+
+export default App;
