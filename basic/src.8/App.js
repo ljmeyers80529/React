522e4519f026fc2ed6474f34884b3dfd189887ef
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-27 14:26:49 -0700

    Passing method reference between components.

---
+++ basic/src.8/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Chomper", age: 3},
+      {name: "Critter", age: 10},
+      {name: "Toffe", age: 1}
+      ],
+      otherState: false
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState( {
+      persons: [
+        {name: newName, age: 5},
+        {name: "Critter", age: 10},
+        {name: "Toffe", age: 1}
+        ]}
+      );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>This is a basic react app.</h1>
+        {/* <button onClick={this.switchNameHandler.bind(this, 'Nuzzy')}>Switch name</button> */}
+        <button onClick={() => this.switchNameHandler('Thor')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Goldy Bear')}>Gray and white tabby cat.</Person>
+
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+export default App;
