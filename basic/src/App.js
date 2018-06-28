50f906ac0e5b6e2e08c29488aa1547e903aba1d9
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-28 14:47:14 -0700

    Modified to use CSS modules by ejecting the normal scripting information

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -38,16 +38,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPerson) {
       persons = (
@@ -63,24 +55,25 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
+
     }
 
     // let classes = ['pColor', 'pBold'].join(' ');       // css class name.
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('pColor');
+      assignedClasses.push(classes.pColor);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('pBold');
+      assignedClasses.push(classes.pBold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>This is a basic react app.</h1>
-        <p className={classes.join(' ')}>This is working!</p>
+        <p className={assignedClasses.join(' ')}>This is working!</p>
         <button 
-        style={style}
+        className={btnClass}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
