6b4369f6efceb9c798e4e49330e047de3402e6b3
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-28 11:44:54 -0700

    Code correction.

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -46,10 +45,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -69,10 +64,6 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     // let classes = ['pColor', 'pBold'].join(' ');       // css class name.
@@ -97,4 +88,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
