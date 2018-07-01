c25f825f1722c0f13d035ff1a508454957174912
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-06-30 21:20:04 -0700

    Progress update.

--- basic/src/components/Cockpit/Cockpit.js
+++ basic/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Cockpit.css'
-import Aux from '../../hoc/Aux';
+import Aux from '../../highOrderComponent/Aux';
 
 const cockpit = (props) => {
   // let classes = ['pColor', 'pBold'].join(' ');       // css class name.
