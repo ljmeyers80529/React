f732429af57c1a14088f14884421bc1219a689e7
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-06 21:14:53 -0700

    Outputting burger ingredients dynamically.

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBulder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBulder.js
@@ -3,10 +3,19 @@ import Auxillary from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad: 1,
+            becon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    };
+
     render() {
         return (
             <Auxillary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build controls.</div>
             </Auxillary>
         );
