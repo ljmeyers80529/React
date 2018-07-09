93f1f2870f7a371579b38e4c44f0084904dce2db
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-09 14:41:06 -0700

    Creating and implementing side drawer menuing system for mobile devices.

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Auxillary from '../../hoc/Auxillary';
-import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Auxillary>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
