ff69b93526e4e90673f29c85d9727616ce0f9a8b
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-10 15:02:42 -0700

    Using interceptors and default global configurations.

--- blogproject/src/index.js
+++ blogproject/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // edit request configuration.
+    return request;                 // must always return with the request information.
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // edit response configuration.
+    return response;                 // must always return with the response information.
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
