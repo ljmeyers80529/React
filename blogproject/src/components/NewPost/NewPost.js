0c9750887fd03e6fdc4a0a6b5bc41e14b79a6ebb
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-10 14:19:58 -0700

    Sending a POST request.

--- blogproject/src/components/NewPost/NewPost.js
+++ blogproject/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,19 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        };
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        .then(response => {
+            console.log(response);
+        });
     }
 
     render () {
@@ -21,8 +35,9 @@ class NewPost extends Component {
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
+                    <option value="Me">Me</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
