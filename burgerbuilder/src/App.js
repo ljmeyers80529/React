3bf2b8e21ee36d7d2865a1e737d7a36701849424
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-14 13:59:45 -0700

    Setup routes and routing.

--- burgerbuilder/src/App.js
+++ burgerbuilder/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBulder';
 import Checkout from './containers/Checkout/Checkout';
@@ -8,8 +10,12 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+          {/* <BurgerBuilder />
+          <Checkout /> */}
         </Layout>
       </div>
     );
