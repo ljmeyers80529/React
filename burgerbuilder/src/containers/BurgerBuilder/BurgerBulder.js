7a7f2e00ba638533ee27c6ddd6c95961e1ae113e
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-18 21:10:38 -0700

    Configure the reducer  for the ingredients.

--- burgerbuilder/src/containers/BurgerBuilder/BurgerBulder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBulder.js
@@ -26,13 +26,13 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
-        axios.get('https://react-burgerbuilder-2d4fc.firebaseio.com/ingredients.json')
-        .then(response => {
-            this.setState({ingredients: response.data})
-        })
-        .catch(error => {
-            this.setState({error: true})
-        });
+        // axios.get('https://react-burgerbuilder-2d4fc.firebaseio.com/ingredients.json')
+        // .then(response => {
+        //     this.setState({ingredients: response.data})
+        // })
+        // .catch(error => {
+        //     this.setState({error: true})
+        // });
     }
 
     updatePurchaeState (ingredients) {
