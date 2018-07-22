223f5159e245444a501b02bceec64916170cbc91
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-21 19:01:16 -0700

    Adding order actions.

--- burgerbuilder/src/store/actions/actionTypes.js
+++ burgerbuilder/src/store/actions/actionTypes.js
@@ -3,3 +3,5 @@ export const REM_INGREDIENT = 'remove_ingredient';
 export const SET_INGREDIENTS = 'set_ingredients';
 export const FETECH_INGREDIENTS_FAILED = 'fetched_ingredients_failed';
 
+export const PURCHASE_BURGER_SUCCESS = 'purchase_burger_success';
+export const PURCHASE_BURGER_FAIL = 'purchase_burger_fail';
