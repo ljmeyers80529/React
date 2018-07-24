e4f5f9ff4535d159a78b01b72c7cb74e750f69d3
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-23 20:32:24 -0700

    Removed commented out code.

--- burgerbuilder/src/containers/Checkout/Checkout.js
+++ burgerbuilder/src/containers/Checkout/Checkout.js
@@ -4,29 +4,8 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-import * as action from '../../store/actions/index';
 
 class Checkout extends Component {
-
-    // state = {
-    //     ingredients: null,
-    //     totalPrice: 0
-    // }
-
-    // componentWillMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     let totalPrice = 0;
-
-    //     for(let param of query.entries()) {
-    //         if (param[0] === 'price') {
-    //             totalPrice = +param[1];
-    //         } else {
-    //             ingredients[param[0]] = +param[1];
-    //         }
-    //     }
-    //     this.setState({ingredients: ingredients, totalPrice: totalPrice});
-    // }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
