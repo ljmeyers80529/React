8e1ee8b4d3805f16724915ec117422d66b995b22
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-26 00:33:53 -0700

    Adding logout link.

--- burgerbuilder/src/App.js
+++ burgerbuilder/src/App.js
@@ -6,6 +6,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBulder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/auth/auth';
+import Logout from './containers/auth/logout/logout';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path="/orders" component={Orders} />
             <Route path="/auth" exact component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
+            <Route path='/logout' exact component={Logout} />
           </Switch>
           {/* <BurgerBuilder />
           <Checkout /> */}
