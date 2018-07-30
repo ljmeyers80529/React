ea5cf18ee8da248192e191e15bf290f39a41171a
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-29 22:49:18 -0700

    Displaying the logged in user's orders.

--- burgerbuilder/src/containers/Orders/Orders.js
+++ burgerbuilder/src/containers/Orders/Orders.js
@@ -16,7 +16,7 @@ class Orders extends Component {
     }
 
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     };
 
     render () {
@@ -43,13 +43,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: (token) => dispatch(action.fetchOrders(token))
+        onFetchOrders: (token, userId) => dispatch(action.fetchOrders(token, userId))
     }
 }
 
