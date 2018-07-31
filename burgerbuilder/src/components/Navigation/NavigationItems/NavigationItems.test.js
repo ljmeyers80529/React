70ba2648d9089c97002605b670d395cab37b8cee
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-31 14:24:51 -0700

    Testing components.

---
+++ burgerbuilder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should render 2 navigation items if not authenticated...', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 navigation items if authenticated...', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should contain a logout navigation item if authenticated...', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
+    });
+});
